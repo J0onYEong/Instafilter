19ae07546775ea0d6325c0a37de492f5ea389b8d
choijun0 <cjy00050005@gmail.com>
2023-01-01 13:48:42 +0900

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by 최준영 on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
