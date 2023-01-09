10c1edb4cd0cbaee4e4421c6ee31405e9e2eb86d
choijun0 <cjy00050005@gmail.com>
2023-01-09 14:49:36 +0900

    Selecting many kinds of filter by using confirmationDialog view. Setting value that is necessary for a filter with explicit key name.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,9 +14,13 @@ struct ContentView: View {
     @State private var inputImage: UIImage?
     @State private var isShowingImagePicker = false
     
-    @State private var intensity = 0.0
+    @State private var intensity = 0.01
     
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilterConfirmationDialog = false
+    
+    @State private var showingEmptyImageAlert = false
+    
     let contenxt = CIContext()
     
     var body: some View {
@@ -43,7 +47,7 @@ struct ContentView: View {
                 
                 VStack {
                     Text("intensity")
-                    Slider(value: $intensity)
+                    Slider(value: $intensity, in: 0.01...1.0)
                         .padding([.trailing, .leading], 20)
                         .onChange(of: intensity) { _ in
                             applyingFilter()
@@ -52,7 +56,7 @@ struct ContentView: View {
                 .padding(.vertical)
                 
                 HStack {
-                    Button("Change Filter") {}
+                    Button("Change Filter") { showingFilterConfirmationDialog = true }
                     Spacer()
                     Button("Save") { save() }
                 }
@@ -67,6 +71,28 @@ struct ContentView: View {
             .onChange(of: inputImage) { _ in
                 loadImage()
             }
+            .confirmationDialog("Set filter", isPresented: $showingFilterConfirmationDialog) {
+                Button("Crystallize") { setFilter(filter: CIFilter.crystallize()) }
+                    .disabled(currentFilter.name == CIFilter.crystallize().name)
+                Button("Edges") { setFilter(filter: CIFilter.edges()) }
+                    .disabled(currentFilter.name == CIFilter.edges().name)
+                Button("Gaussian Blur") { setFilter(filter: CIFilter.gaussianBlur()) }
+                    .disabled(currentFilter.name == CIFilter.gaussianBlur().name)
+                Button("Pixellate") { setFilter(filter: CIFilter.pixellate()) }
+                    .disabled(currentFilter.name == CIFilter.pixellate().name)
+                Button("Sepia Tone") { setFilter(filter: CIFilter.sepiaTone()) }
+                    .disabled(currentFilter.name == CIFilter.sepiaTone().name)
+                Button("Unsharp Mask") { setFilter(filter: CIFilter.unsharpMask()) }
+                    .disabled(currentFilter.name == CIFilter.unsharpMask().name)
+                Button("Vignette") { setFilter(filter: CIFilter.vignette()) }
+                    .disabled(currentFilter.name == CIFilter.vignette().name)
+                Button("Cancel", role: .cancel) { }
+            }
+            .alert("Empty Image", isPresented: $showingEmptyImageAlert) {
+                Button("Ok") {}
+            } message: {
+                Text("select image before filtering")
+            }
         }
     }
     
@@ -74,9 +100,16 @@ struct ContentView: View {
         
     }
     
+    func setFilter(filter: CIFilter) {
+        currentFilter = filter
+        intensity = 0.01
+        loadImage()
+    }
+    
     func loadImage() {
         guard let inputImg = inputImage else {
-            fatalError("Image doesn't exist")
+            showingEmptyImageAlert = true
+            return
         }
         let ciImg = CIImage(image: inputImg)
         currentFilter.setValue(ciImg, forKey: kCIInputImageKey)
@@ -84,10 +117,15 @@ struct ContentView: View {
     }
     
     func applyingFilter() {
-        currentFilter.setValue(Float(intensity), forKey: kCIInputIntensityKey)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity * 10, forKey: kCIInputScaleKey) }
+        
         guard let output = currentFilter.outputImage else {
             return
         }
+        
         if let cgImg = contenxt.createCGImage(output, from: output.extent) {
             let uiImg = UIImage(cgImage: cgImg)
             self.image = Image(uiImage: uiImg)
